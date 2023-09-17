b35b643821feaaaf90ec408231e439474cea6912
javanance <javanance@gmail.com>
2023-09-17 15:40:19 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 const withNextra = require("nextra")({
   output: "export",
-  images.unoptimized = true,
+  images: {
+    unoptimized: true,
+  },
   theme: "nextra-theme-docs",
   themeConfig: "./theme.config.tsx",
   latex: true,
